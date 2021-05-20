b66b29ffd66f60808840e2f23c6ccf0825325441
Giovanni <giovanni.bartolomeo.rg@gmail.com>
2021-05-20 14:52:48 +0200

    updated build script

---
+++ node_engine/NetManager/build.sh
@@ -0,0 +1,6 @@
+env GOOS=linux GOARCH=arm GOARM=7 go build -o bin/arm-7-TwoNetnsDev1 testEnvironment/TwoNetnsDev1.go
+env GOOS=linux GOARCH=arm GOARM=7 go build -o bin/arm-7-TwoNetnsDev2 testEnvironment/TwoNetnsDev2.go
+env GOOS=linux GOARCH=arm GOARM=7 go build -o bin/arm-7-TestDockerDev1 testEnvironment/TestDockerDev1.go
+env GOOS=linux GOARCH=amd64 go build -o bin/amd64-TwoNetnsDev1 testEnvironment/TwoNetnsDev1.go
+env GOOS=linux GOARCH=amd64 go build -o bin/amd64-TwoNetnsDev2 testEnvironment/TwoNetnsDev2.go
+env GOOS=linux GOARCH=amd64 go build -o bin/amd64-TestDockerDev1 testEnvironment/TestDockerDev1.go
