7c632f21e54888aa35947c0903870e5d98d93097
Giovanni <giovanni.bartolomeo.rg@gmail.com>
2021-05-26 19:22:26 +0200

    fixed namespace_ip update

--- cluster_orchestrator/cluster-manager/mongodb_client.py
+++ cluster_orchestrator/cluster-manager/mongodb_client.py
@@ -189,7 +189,7 @@ def mongo_update_job_deployed(job_id, status, ns_ip, node_id):
     job = mongo_jobs.db.jobs.find_one({'_id': ObjectId(job_id)})
     instance_list = job['instance_list']
     for instance in instance_list:
-        if str(instance.get('worker_id')) is str(node_id) and instance.get('namespace_ip') is '':
+        if str(instance.get('worker_id')) == str(node_id) and instance.get('namespace_ip') is '':
             instance['namespace_ip'] = ns_ip
             break
     return mongo_jobs.db.jobs.update_one({'_id': ObjectId(job_id)},
