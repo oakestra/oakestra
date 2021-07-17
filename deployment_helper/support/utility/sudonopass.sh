897ee03136d6cf415bc86e3fab0c1c9382ee2a51
Giovanni <giovanni.bartolomeo.rg@gmail.com>
2021-07-17 16:22:37 +0200

    updated sudonopass

--- deployment_helper/support/utility/sudonopass.sh
+++ deployment_helper/support/utility/sudonopass.sh
@@ -1,12 +1,13 @@
 res=$(cat /etc/sudoers.d/edgeio  | grep -c $1)
 
-if [[ $res -gt 0 ]]; then
-        echo "already set up"
-	exit
-fi
+#if [[ $res -gt 0 ]]; then
+#        echo "already set up"
+#	exit
+#fi
 
 echo "no sudo setuo found for current user"
 echo "adding current user to /etc/sudoers.d/edgeio"
 sudo -S sh -c "echo '$1   ALL=(ALL:ALL) NOPASSWD:ALL' >> /etc/sudoers.d/edgeio"
-sudo -S sh -c "mv /etc/sudoers.d/groupsettings /etc/sudoers.d/groupsettings.old"
+sudo -S sh -c "echo '$1   ALL=(ALL:ALL) NOPASSWD:ALL' >> /etc/sudoers.d/groupsettings"
+#sudo -S sh -c "mv /etc/sudoers.d/groupsettings /etc/sudoers.d/groupsettings.old"
 echo "SUCCESS!! :)"
