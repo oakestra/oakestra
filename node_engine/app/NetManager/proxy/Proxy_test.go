45543c4778e46722df88e6ddeefacbfd26d4d0cd
OliHalu <33519938+OliHalu@users.noreply.github.com>
2022-01-04 10:47:11 +0100

    clean up

---
+++ node_engine/app/NetManager/proxy/Proxy_test.go
@@ -0,0 +1,193 @@
+package proxy
+
+import (
+	"NetManager/env"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"net"
+	"testing"
+)
+
+type FakeEnv struct {
+}
+
+func (fakeenv *FakeEnv) GetTableEntryByServiceIP(ip net.IP) []env.TableEntry {
+	entrytable := make([]env.TableEntry, 0)
+	//If entry already available
+	entry := env.TableEntry{
+		Appname:          "a",
+		Appns:            "a",
+		Servicename:      "b",
+		Servicenamespace: "b",
+		Instancenumber:   0,
+		Cluster:          0,
+		Nodeip:           net.ParseIP("10.0.0.1"),
+		Nsip:             net.ParseIP("172.19.2.12"),
+		ServiceIP: []env.ServiceIP{{
+			IpType:  env.Closest,
+			Address: net.ParseIP("172.30.255.255"),
+		},
+			{
+				IpType:  env.InstanceNumber,
+				Address: net.ParseIP("172.30.255.254"),
+			}},
+	}
+	entrytable = append(entrytable, entry)
+	return entrytable
+}
+
+func (fakeenv *FakeEnv) GetTableEntryByNsIP(ip net.IP) (env.TableEntry, bool) {
+	entry := env.TableEntry{
+		Appname:          "a",
+		Appns:            "a",
+		Servicename:      "c",
+		Servicenamespace: "b",
+		Instancenumber:   0,
+		Cluster:          0,
+		Nodeip:           net.ParseIP("10.0.0.1"),
+		Nsip:             net.ParseIP("172.19.1.1"),
+		ServiceIP: []env.ServiceIP{{
+			IpType:  env.Closest,
+			Address: net.ParseIP("172.30.255.252"),
+		},
+			{
+				IpType:  env.InstanceNumber,
+				Address: net.ParseIP("172.30.255.253"),
+			}},
+	}
+	return entry, true
+}
+
+func (fakeenv *FakeEnv) GetTableEntryByInstanceIP(ip net.IP) (env.TableEntry, bool) {
+	return env.TableEntry{}, false
+}
+
+func getFakeTunnel() GoProxyTunnel {
+	tunnel := GoProxyTunnel{
+		tunNetIP:    "172.19.1.254",
+		ifce:        nil,
+		isListening: true,
+		ProxyIpSubnetwork: net.IPNet{
+			IP:   net.ParseIP("172.30.0.0"),
+			Mask: net.IPMask(net.ParseIP("255.255.0.0").To4()),
+		},
+		HostTUNDeviceName: "goProxyTun",
+		TunnelPort:        50011,
+		listenConnection:  nil,
+		proxycache:        NewProxyCache(),
+	}
+	tunnel.SetEnvironment(&FakeEnv{})
+	return tunnel
+}
+
+func getFakePacket(srcIP string, dstIP string, srcPort int, dstPort int) gopacket.Packet {
+	ipLayer := layers.IPv4{
+		SrcIP:    net.ParseIP(srcIP),
+		DstIP:    net.ParseIP(dstIP),
+		Protocol: layers.IPProtocolTCP,
+	}
+	tcpLayer := layers.TCP{
+		SrcPort: layers.TCPPort(srcPort),
+		DstPort: layers.TCPPort(dstPort),
+		SYN:     true,
+	}
+	buf := gopacket.NewSerializeBuffer()
+	opts := gopacket.SerializeOptions{
+		FixLengths:       true,
+		ComputeChecksums: false,
+	}
+	_ = gopacket.SerializeLayers(buf, opts, &ipLayer, &tcpLayer)
+	return gopacket.NewPacket(buf.Bytes(), layers.LayerTypeIPv4, gopacket.Default)
+}
+
+func TestOutgoingProxy(t *testing.T) {
+	proxy := getFakeTunnel()
+
+	proxypacket := getFakePacket("172.19.1.1", "172.30.255.255", 666, 80)
+	noproxypacket := getFakePacket("172.19.1.1", "172.20.1.1", 666, 80)
+
+	newpacketproxy := proxy.outgoingProxy(proxypacket)
+	newpacketnoproxy := proxy.outgoingProxy(noproxypacket)
+
+	if ipLayer := newpacketproxy.Layer(layers.LayerTypeIPv4); ipLayer != nil {
+		if tcpLayer := newpacketproxy.Layer(layers.LayerTypeTCP); tcpLayer != nil {
+
+			ipv4, _ := ipLayer.(*layers.IPv4)
+			dstexpected := net.ParseIP("172.19.2.12")
+			if !ipv4.DstIP.Equal(dstexpected) {
+				t.Error("dstIP = ", ipv4.DstIP.String(), "; want =", dstexpected)
+			}
+			//tcp, _ := tcpLayer.(*layers.TCP)
+			//if !(tcp.SrcPort == layers.TCPPort(proxy.TunnelPort)) {
+			//	t.Error("srcPort = ", tcp.SrcPort.String(), "; want = ", proxy.TunnelPort)
+			//}
+		}
+	}
+	if ipLayer := newpacketnoproxy.Layer(layers.LayerTypeIPv4); ipLayer != nil {
+		if tcpLayer := newpacketnoproxy.Layer(layers.LayerTypeTCP); tcpLayer != nil {
+
+			ipv4, _ := ipLayer.(*layers.IPv4)
+			dstexpected := net.ParseIP("172.20.1.1")
+			if !ipv4.DstIP.Equal(dstexpected) {
+				t.Error("dstIP = ", ipv4.DstIP.String(), "; want =", dstexpected)
+			}
+
+			tcp, _ := tcpLayer.(*layers.TCP)
+			if !(tcp.SrcPort == layers.TCPPort(666)) {
+				t.Error("srcPort = ", tcp.SrcPort.String(), "; want = ", 666)
+			}
+		}
+	}
+}
+
+func TestIngoingProxy(t *testing.T) {
+	proxy := getFakeTunnel()
+
+	proxypacket := getFakePacket("172.30.0.5", "172.19.1.15", 666, 777)
+	noproxypacket := getFakePacket("172.19.2.1", "172.19.1.12", 666, 80)
+
+	//update proxy proxycache
+	entry := ConversionEntry{
+		srcip:         net.ParseIP("172.19.1.15"),
+		dstip:         net.ParseIP("172.19.2.1"),
+		dstServiceIp:  net.ParseIP("172.30.255.255"),
+		srcInstanceIp: net.ParseIP("172.30.0.50"),
+		srcport:       777,
+		dstport:       666,
+	}
+	proxy.proxycache.Add(entry)
+
+	newpacketproxy := proxy.ingoingProxy(proxypacket)
+	newpacketnoproxy := proxy.ingoingProxy(noproxypacket)
+
+	if ipLayer := newpacketproxy.Layer(layers.LayerTypeIPv4); ipLayer != nil {
+		if tcpLayer := newpacketproxy.Layer(layers.LayerTypeTCP); tcpLayer != nil {
+
+			ipv4, _ := ipLayer.(*layers.IPv4)
+			srcexpected := net.ParseIP("172.30.255.255")
+			if !ipv4.SrcIP.Equal(srcexpected) {
+				t.Error("srcIp = ", ipv4.SrcIP.String(), "; want =", srcexpected)
+			}
+
+			//tcp, _ := tcpLayer.(*layers.TCP)
+			//if !(int(tcp.DstPort) == entry.srcport) {
+			//	t.Error("dstPort = ", int(tcp.DstPort), "; want = ", entry.srcport)
+			//}
+		}
+	}
+	if ipLayer := newpacketnoproxy.Layer(layers.LayerTypeIPv4); ipLayer != nil {
+		if tcpLayer := newpacketnoproxy.Layer(layers.LayerTypeTCP); tcpLayer != nil {
+
+			ipv4, _ := ipLayer.(*layers.IPv4)
+			srcexpected := net.ParseIP("172.19.2.1")
+			if !ipv4.SrcIP.Equal(srcexpected) {
+				t.Error("dstIP = ", ipv4.SrcIP.String(), "; want =", srcexpected)
+			}
+
+			tcp, _ := tcpLayer.(*layers.TCP)
+			if !(tcp.DstPort == layers.TCPPort(80)) {
+				t.Error("srcPort = ", tcp.DstPort.String(), "; want = ", 80)
+			}
+		}
+	}
+}
