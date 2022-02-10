d2e2fd1b671ec659060f772e31e8c57852e176c4
OliHalu <33519938+OliHalu@users.noreply.github.com>
2022-02-10 17:59:03 +0100

    adapt instance list after migration due to sla violation

--- cluster_orchestrator/cluster-manager/mqtt_client.py
+++ cluster_orchestrator/cluster-manager/mqtt_client.py
@@ -186,7 +186,7 @@ def handle_nodes_alarm_topic(data):
             node_id = instance.get('worker_id')
             if node_id == client_id:
                 # Remove worker id from instance
-                del job["instance_list"][i]
+                del job["instance_list"][i]["worker_id"]
         app.logger.info(f"Update Job: {job}")
         mongo_upsert_job(job)
         mqtt_publish_edge_delete(client_id, job)
