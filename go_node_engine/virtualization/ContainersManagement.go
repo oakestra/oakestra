41afff98f6a3d6ee4a967834d8d1ce1738577f85
giobarty <giovanni.bartolomeo.rg@gmail.com>
2022-03-11 17:05:01 +0100

    Fixed dns

--- go_node_engine/virtualization/ContainersManagement.go
+++ go_node_engine/virtualization/ContainersManagement.go
@@ -6,11 +6,15 @@ import (
 	"fmt"
 	"github.com/containerd/containerd"
 	"github.com/containerd/containerd/cio"
+	"github.com/containerd/containerd/containers"
 	"github.com/containerd/containerd/namespaces"
 	"github.com/containerd/containerd/oci"
+	"github.com/opencontainers/runtime-spec/specs-go"
 	"go_node_engine/logger"
 	"go_node_engine/model"
 	"go_node_engine/requests"
+	"io/ioutil"
+	"os"
 	"reflect"
 	"sync"
 	"time"
@@ -133,17 +137,42 @@ func (r *ContainerRuntime) containerCreationRoutine(
 	killChannel *chan bool,
 ) {
 
+	revert := func(err error) {
+		startup <- false
+		errorchan <- err
+	}
+	//create container general oci specs
+	hostname := fmt.Sprintf("%s.instance", sname)
+	specs := []oci.SpecOpts{
+		oci.WithImageConfig(image),
+		oci.WithHostHostsFile,
+		oci.WithHostname(hostname),
+		oci.WithEnv([]string{fmt.Sprintf("HOSTNAME=%s", hostname)}),
+	}
+	//add user defined commands
+	if len(cmd) > 0 {
+		specs = append(specs, oci.WithProcessArgs(cmd...))
+	}
+	//add resolve file with default google dns
+	resolvconfFile, err := getGoogleDNSResolveConf()
+	if err != nil {
+		revert(err)
+		return
+	}
+	defer resolvconfFile.Close()
+	_ = resolvconfFile.Chmod(444)
+	specs = append(specs, withCustomResolvConf(resolvconfFile.Name()))
+
 	// create the container
 	container, err := r.contaierClient.NewContainer(
 		ctx,
 		sname,
 		containerd.WithImage(image),
 		containerd.WithNewSnapshot(fmt.Sprintf("%s-snapshotter", sname), image),
-		containerd.WithNewSpec(oci.WithImageConfig(image)),
+		containerd.WithNewSpec(specs...),
 	)
 	if err != nil {
-		startup <- false
-		errorchan <- err
+		revert(err)
 		return
 	}
 	defer container.Delete(ctx)
@@ -152,8 +181,7 @@ func (r *ContainerRuntime) containerCreationRoutine(
 	task, err := container.NewTask(ctx, cio.NewCreator(cio.WithStdio))
 	if err != nil {
 		logger.ErrorLogger().Printf("ERROR: containerd task creation failure: %v", err)
-		startup <- false
-		errorchan <- err
+		revert(err)
 		return
 	}
 	defer task.Delete(ctx)
@@ -164,8 +192,7 @@ func (r *ContainerRuntime) containerCreationRoutine(
 		err = requests.AttachNetworkToTask(taskpid, sname, 0)
 		if err != nil {
 			logger.ErrorLogger().Printf("Unable to attach network interface to the task: %v", err)
-			startup <- false
-			errorchan <- err
+			revert(err)
 			return
 		}
 	}
@@ -174,17 +201,14 @@ func (r *ContainerRuntime) containerCreationRoutine(
 	exitStatusC, err := task.Wait(ctx)
 	if err != nil {
 		logger.ErrorLogger().Printf("ERROR: containerd task wait failure: %v", err)
-		startup <- false
-		errorchan <- err
+		revert(err)
 		return
 	}
 
 	// execute the image's task
-
 	if err := task.Start(ctx); err != nil {
 		logger.ErrorLogger().Printf("ERROR: containerd task start failure: %v", err)
-		startup <- false
-		errorchan <- err
+		revert(err)
 		return
 	}
 
@@ -222,4 +246,30 @@ func (r *ContainerRuntime) containerCreationRoutine(
 	r.channelLock.Lock()
 	defer r.channelLock.Unlock()
 	r.killQueue[sname] = nil
+}
+
+func withCustomResolvConf(src string) func(context.Context, oci.Client, *containers.Container, *oci.Spec) error {
+	return func(_ context.Context, _ oci.Client, _ *containers.Container, s *oci.Spec) error {
+		s.Mounts = append(s.Mounts, specs.Mount{
+			Destination: "/etc/resolv.conf",
+			Type:        "bind",
+			Source:      src,
+			Options:     []string{"rbind", "ro"},
+		})
+		return nil
+	}
+}
+
+func getGoogleDNSResolveConf() (*os.File, error) {
+	file, err := ioutil.TempFile("/tmp", "edgeio-resolv-conf")
+	if err != nil {
+		logger.ErrorLogger().Printf("Unable to create temp resolv file: %v", err)
+		return nil, err
+	}
+	_, err = file.WriteString(fmt.Sprintf("nameserver 8.8.8.8\n"))
+	if err != nil {
+		logger.ErrorLogger().Printf("Unable to write temp resolv file: %v", err)
+		return nil, err
+	}
+	return file, err
 }
