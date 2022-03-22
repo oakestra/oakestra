086103b366b2806a59a0f238c6a799319a2b07d7
giobarty <giovanni.bartolomeo.rg@gmail.com>
2022-03-22 17:24:15 +0100

    Adapted port mappings for compatibility with a single node deployment

--- go_node_engine/build/build.sh
+++ go_node_engine/build/build.sh
@@ -1,5 +1,5 @@
 #arm build
-env GOOS=linux GOARCH=arm GOARM=7 go build -o bin/NodeEngine_arm ../NodeEngine.go
+env GOOS=linux GOARCH=arm GOARM=7 go build -o bin/NodeEngine_arm-7 ../NodeEngine.go
 
 #amd build
 env GOOS=linux GOARCH=amd64 go build -o bin/NodeEngine_amd64 ../NodeEngine.go
