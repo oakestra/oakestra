e55dabd8df852b717ceff7b08760ca72f84fd420
giobarty <giovanni.bartolomeo.rg@gmail.com>
2023-10-10 16:38:27 +0200

    Added nvidia GPU metrics

--- go_node_engine/model/Node.go
+++ go_node_engine/model/Node.go
@@ -2,7 +2,7 @@ package model
 
 import (
 	"fmt"
-	"github.com/jaypipes/ghw"
+	"github.com/NVIDIA/gpu-monitoring-tools/bindings/go/nvml"
 	"github.com/shirou/gopsutil/cpu"
 	"github.com/shirou/gopsutil/disk"
 	"github.com/shirou/gopsutil/host"
@@ -33,7 +33,11 @@ type Node struct {
 	MemoryMB       int               `json:"memory_free_in_MB"`
 	DiskInfo       map[string]string `json:"disk_info"`
 	NetworkInfo    map[string]string `json:"network_info"`
-	GpuInfo        map[string]string `json:"gpu_info"`
+	GpuDriver      string            `json:"gpu_driver"`
+	GpuUsage       float64           `json:"gpu_usage"`
+	GpuTemp        float64           `json:"gpu_temp"`
+	GpuMemUsage    float64           `json:"gpu_mem_used"`
+	GpuTotMem      float64           `json:"gpu_tot_mem"`
 	Technology     []string          `json:"technology"`
 	Overlay        bool
 	NetManagerPort int
@@ -60,10 +64,15 @@ func GetNodeInfo() Node {
 func GetDynamicInfo() Node {
 	node.updateDynamicInfo()
 	return Node{
-		CpuUsage:   node.CpuUsage,
-		CpuCores:   node.CpuCores,
-		MemoryUsed: node.MemoryUsed,
-		MemoryMB:   node.MemoryMB,
+		CpuUsage:    node.CpuUsage,
+		CpuCores:    node.CpuCores,
+		MemoryUsed:  node.MemoryUsed,
+		MemoryMB:    node.MemoryMB,
+		GpuDriver:   node.GpuDriver,
+		GpuTemp:     node.GpuTemp,
+		GpuUsage:    node.GpuUsage,
+		GpuTotMem:   node.GpuTotMem,
+		GpuMemUsage: node.GpuMemUsage,
 	}
 }
 
@@ -73,13 +82,30 @@ func EnableOverlay(port int) {
 }
 
 func (n *Node) updateDynamicInfo() {
+	// System Info
 	n.CpuUsage = getAvgCpuUsage()
 	n.Ip = getIp()
 	n.MemoryMB = getMemoryMB()
 	n.MemoryUsed = getMemoryUsage()
 	n.DiskInfo = getDiskinfo()
 	n.NetworkInfo = getNetworkInfo()
-	n.GpuInfo = getGpuInfo()
+
+	// GPU Info
+	err := nvml.Init()
+	if err != nil {
+		n.GpuDriver = "None"
+		n.GpuTemp = 0
+		n.GpuUsage = 0
+		n.GpuMemUsage = 0
+		n.GpuTotMem = 0
+	}
+	defer nvml.Shutdown()
+
+	n.GpuDriver = getGpuDriver()
+	n.GpuTotMem = getTotGpuMem()
+	n.GpuMemUsage = getGpuMemUsage()
+	n.GpuUsage = getGpuUsage()
+	n.GpuTemp = getGpuTemp()
 }
 
 func SetNodeId(id string) {
@@ -179,19 +205,6 @@ func getDiskinfo() map[string]string {
 	return diskInfoMap
 }
 
-func getGpuInfo() map[string]string {
-	gpu, err := ghw.GPU()
-	gpuInfoMap := make(map[string]string)
-	if err != nil {
-		fmt.Printf("Error %v", err)
-		return gpuInfoMap
-	}
-	for i, card := range gpu.GraphicsCards {
-		gpuInfoMap[fmt.Sprintf("gpu_%d", i)] = card.String()
-	}
-	return gpuInfoMap
-}
-
 func getNetworkInfo() map[string]string {
 	netInfoMap := make(map[string]string)
 	interfaces, err := psnet.Interfaces()
@@ -213,4 +226,94 @@ func getPort() string {
 
 func getSupportedTechnologyList() []string {
 	return []string{CONTAINER_RUNTIME}
+}
+
+func getGpuDriver() string {
+	version, err := nvml.GetDriverVersion()
+	if err != nil {
+		return ""
+	}
+
+	return version
+}
+
+func getGpuMemUsage() float64 {
+
+	count, err := nvml.GetDeviceCount()
+	if err != nil {
+		return 0.0
+	}
+
+	totMem := 0.0
+	for i := uint(0); i < count; i++ {
+		device, err := nvml.NewDevice(i)
+
+		status, err := device.Status()
+		if err != nil {
+			return 0.0
+		}
+		totMem += float64(*status.Utilization.Memory)
+
+	}
+	return totMem / float64(count)
+}
+
+func getGpuUsage() float64 {
+	count, err := nvml.GetDeviceCount()
+	if err != nil {
+		return 0.0
+	}
+
+	totUage := 0.0
+	for i := uint(0); i < count; i++ {
+		device, err := nvml.NewDevice(i)
+
+		status, err := device.Status()
+		if err != nil {
+			return 0.0
+		}
+		totUage += float64(*status.Utilization.GPU)
+
+	}
+	return totUage / float64(count)
+}
+
+func getTotGpuMem() float64 {
+	count, err := nvml.GetDeviceCount()
+	if err != nil {
+		return 0.0
+	}
+
+	totMem := 0.0
+	for i := uint(0); i < count; i++ {
+		device, err := nvml.NewDevice(i)
+
+		status, err := device.Status()
+		if err != nil {
+			return 0.0
+		}
+		totMem += float64(*status.Memory.Global.Free + *status.Memory.Global.Used)
+
+	}
+	return totMem
+}
+
+func getGpuTemp() float64 {
+	count, err := nvml.GetDeviceCount()
+	if err != nil {
+		return 0.0
+	}
+
+	totTemp := 0.0
+	for i := uint(0); i < count; i++ {
+		device, err := nvml.NewDevice(i)
+
+		status, err := device.Status()
+		if err != nil {
+			return 0.0
+		}
+		totTemp += float64(*status.Temperature)
+
+	}
+	return totTemp / float64(count)
 }
