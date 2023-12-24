653daaa587f6c022614071e15e23f1a649bf34ec
Mahmoud ElKodary <melkodary@outlook.com>
2023-12-24 14:18:20 +0100

    quick bug fix

--- root_orchestrator/resource-abstractor/db/clusters_db.py
+++ root_orchestrator/resource-abstractor/db/clusters_db.py
@@ -13,9 +13,9 @@ def find_clusters(filter):
     return db.mongo_clusters.db.clusters.aggregate(pipeline)
 
 def find_cluster_by_id(cluster_id):
-    cluster = find_clusters({"_id": ObjectId(cluster_id)})
+    cluster = list(find_clusters({"_id": ObjectId(cluster_id)}))
     return cluster[0] if cluster else None
 
 def find_cluster_by_name(name):
-    cluster = find_clusters({"cluster_name": name})
+    cluster = list(find_clusters({"cluster_name": name}))
     return cluster[0] if cluster else None
