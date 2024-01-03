fd6b48887a66af31662412fff2a131ec10b69e2b
Giovanni Bartolomeo <giobarty@gmail.com>
2024-01-03 19:17:34 +0100

    fixed application history

--- root_orchestrator/system-manager-python/ext_requests/apps_db.py
+++ root_orchestrator/system-manager-python/ext_requests/apps_db.py
@@ -46,9 +46,9 @@ def mongo_update_job_status(job_id, status, status_detail, instances=None):
     job.get("instance_list")
     if instances is not None:
         for instance in instances:
-            # current_time = datetime.now().isoformat()
-            # cpu_update = {"value": instance.get("cpu"), "timestamp": current_time}
-            # memory_update = {"value": instance.get("memory"), "timestamp": current_time}
+            current_time = datetime.now().isoformat()
+            cpu_update = {"value": instance.get("cpu"), "timestamp": current_time}
+            memory_update = {"value": instance.get("memory"), "timestamp": current_time}
             db.mongo_services.update_one(
                 {"_id": ObjectId(job_id),
                  "instance_list": {"$elemMatch": {"instance_number": instance["instance_number"]}}},
