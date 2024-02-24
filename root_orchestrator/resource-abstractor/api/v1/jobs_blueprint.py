9ede9e10d22133197b405aee683175a7b9da7935
Mahmoud ElKodary <melkodary@outlook.com>
2024-02-24 16:09:27 +0100

    no schema needed

--- root_orchestrator/resource-abstractor/api/v1/jobs_blueprint.py
+++ root_orchestrator/resource-abstractor/api/v1/jobs_blueprint.py
@@ -1,40 +1,20 @@
-from bson.objectid import ObjectId
+from bson import ObjectId, json_util
 from db.jobs_db import find_all_jobs, find_job_by_id, update_job
 from flask.views import MethodView
 from flask_smorest import Blueprint
-from marshmallow import Schema, fields
 from werkzeug import exceptions
 
 jobsblp = Blueprint("Jobs Api", "jobs_api", url_prefix="/api/v1/jobs")
 
 
-class JobSchema(Schema):
-    id = fields.String(attribute="_id")
-    system_job_id = fields.String()
-    job_name = fields.String()
-    service_name = fields.String()
-    code = fields.String()
-    cmd = fields.List(fields.String())
-    status = fields.String()
-    replicas = fields.Integer()
-    instance_list = fields.List(fields.Dict())
-    service_ip_list = fields.List(fields.Dict())
-    last_modified_timestamp = fields.Float()
-    cluster = fields.String()
-    status = fields.String()
-
-
 @jobsblp.route("/")
 class AllJobsController(MethodView):
-    @jobsblp.response(200, JobSchema(many=True), content_type="application/json")
     def get(self):
-        # TODO: support pagination
-        return list(find_all_jobs())
+        return json_util.dumps(find_all_jobs())
 
 
 @jobsblp.route("/<jobId>")
 class JobController(MethodView):
-    @jobsblp.response(200, JobSchema(), content_type="application/json")
     def get(self, jobId):
         if ObjectId.is_valid(jobId) is False:
             raise exceptions.BadRequest()
@@ -45,8 +25,6 @@ class JobController(MethodView):
 
         return job
 
-    @jobsblp.arguments(JobSchema(), location="json")
-    @jobsblp.response(200, content_type="application/json")
     def patch(self, *args, **kwargs):
         job_id = kwargs["jobId"]
         data = None
--- root_orchestrator/resource-abstractor/api/v1/resources_blueprint.py
+++ root_orchestrator/resource-abstractor/api/v1/resources_blueprint.py
@@ -1,4 +1,4 @@
-from bson.objectid import ObjectId
+from bson import ObjectId, json_util
 from db.clusters_db import find_cluster_by_id, find_clusters
 from db.clusters_helper import build_filter
 from db.jobs_db import find_job_by_id
@@ -10,25 +10,6 @@ from werkzeug import exceptions
 resourcesblp = Blueprint("Resources Info", "resources_info", url_prefix="/api/v1/resources")
 
 
-class ResourceSchema(Schema):
-    _id = fields.String()
-    cluster_name = fields.String()
-    cluster_location = fields.String()
-    ip = fields.String()
-    port = fields.String()
-    active_nodes = fields.Integer()
-    active = fields.Boolean()
-
-    memory_in_mb = fields.Integer()
-    total_cpu_cores = fields.Integer()
-    total_gpu_cores = fields.Integer()
-    aggregated_cpu_percent = fields.Float()
-    available_memory = fields.Float()
-    total_gpu_percent = fields.Integer()
-    virtualization = fields.List(fields.String())
-    last_modified_timestamp = fields.Float()
-
-
 class ResourceFilterSchema(Schema):
     active = fields.Boolean()
     job_id = fields.String()
@@ -38,7 +19,6 @@ class ResourceFilterSchema(Schema):
 @resourcesblp.route("/")
 class AllResourcesController(MethodView):
     @resourcesblp.arguments(ResourceFilterSchema, location="query")
-    @resourcesblp.response(200, ResourceSchema(many=True), content_type="application/json")
     def get(self, query={}):
         filter = query
         job_id = filter.get("job_id")
@@ -57,12 +37,11 @@ class AllResourcesController(MethodView):
             filter["cluster_id"] = cluster_id
         filter = build_filter(query)
 
-        return list(find_clusters(filter))
+        return json_util.dumps(find_clusters(filter))
 
 
 @resourcesblp.route("/<resourceId>")
 class ResourceController(MethodView):
-    @resourcesblp.response(200, ResourceSchema(), content_type="application/json")
     def get(self, resourceId):
         if ObjectId.is_valid(resourceId) is False:
             raise exceptions.BadRequest()
@@ -71,4 +50,4 @@ class ResourceController(MethodView):
         if cluster is None:
             raise exceptions.NotFound()
 
-        return cluster
+        return json_util.dumps(cluster)
