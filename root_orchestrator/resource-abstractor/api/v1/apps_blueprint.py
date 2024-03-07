c99c312b36b46d02dfd780f7c237dee9322d298a
Mahmoud ElKodary <melkodary@outlook.com>
2024-03-07 17:58:44 +0100

    quick app api check

--- root_orchestrator/resource-abstractor/api/v1/apps_blueprint.py
+++ root_orchestrator/resource-abstractor/api/v1/apps_blueprint.py
@@ -1,10 +1,10 @@
-from bson import json_util
+import json
+
 from db import apps_db
 from flask import request
 from flask.views import MethodView
 from flask_restful import Resource
 from flask_smorest import Blueprint
-from marshmallow import INCLUDE, Schema, fields
 
 applicationblp = Blueprint(
     "Application operations",
@@ -14,57 +14,34 @@ applicationblp = Blueprint(
 )
 
 
-class ApplicationSchema(Schema):
-    _id = fields.String()
-
-
 @applicationblp.route("/")
 class ApplicationsController(Resource):
-    @applicationblp.response(
-        200, ApplicationSchema(unknown=INCLUDE), content_type="application/json"
-    )
     def get(self, *args, **kwargs):
-        return apps_db.find_all_apps()
+        return json.dumps(list(apps_db.find_all_apps(), default=str))
 
 
 @applicationblp.route("/<userid>")
 class CreateApplicationController(Resource):
-    @applicationblp.response(
-        200, ApplicationSchema(unknown=INCLUDE), content_type="application/json"
-    )
     def get(self, userid, *args, **kwargs):
-        return apps_db.find_user_apps(userid)
+        return json.dumps(list(apps_db.find_user_apps(userid)), default=str)
 
-    @applicationblp.response(
-        200, ApplicationSchema(unknown=INCLUDE), content_type="application/json"
-    )
     def post(self, userid, *args, **kwargs):
         data = request.get_json()
-        return apps_db.create_app(userid, data)
+        return json.dump(apps_db.create_app(userid, data), default=str)
 
 
 @applicationblp.route("/<userid>/<appid>")
 class ApplicationController(MethodView):
-    @applicationblp.response(
-        200, ApplicationSchema(unknown=INCLUDE), content_type="application/json"
-    )
     def get(self, userid, appid, *args, **kwargs):
-        return apps_db.find_user_app(userid, appid)
+        return json.dump(apps_db.find_user_app(userid, appid), default=str)
 
-    @applicationblp.response(200, content_type="application/json")
     def delete(self, userid, appid, *args, **kwargs):
-        return json_util.dumps(apps_db.delete_app(userid, appid))
+        return json.dump(apps_db.delete_app(userid, appid), default=str)
 
-    @applicationblp.response(
-        200, ApplicationSchema(unknown=INCLUDE), content_type="application/json"
-    )
     def put(self, userid, appid, *args, **kwargs):
         data = request.get_json()
-        return apps_db.update_app(userid, appid, data)
+        return json.dump(apps_db.update_app(userid, appid, data), default=str)
 
-    @applicationblp.response(
-        200, ApplicationSchema(unknown=INCLUDE), content_type="application/json"
-    )
     def patch(self, userid, appid, *args, **kwargs):
         data = request.get_json()
-        return apps_db.update_app(userid, appid, data)
+        return json.dump(apps_db.update_app(userid, appid, data), default=str)
---
+++ root_orchestrator/resource-abstractor/tests/apps_test.py
@@ -0,0 +1,38 @@
+import json
+import unittest
+from unittest.mock import patch
+
+from api.v1.apps_blueprint import applicationblp  # noqa: E402 F401
+from bson import ObjectId
+from flask import Flask  # noqa: E402
+
+# Add the parent directory to sys.path
+
+
+class BlueprintTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = Flask(__name__)
+        self.app.register_blueprint(applicationblp)
+        self.client = self.app.test_client()
+
+    @patch("api.v1.apps_blueprint.apps_db.find_all_apps")
+    def test_get_all_apps(self, mock_find_all_apps):
+        expected = [
+            {"_id": "65d200f3812caeb85e21ee19", "application_name": "app1"},
+            {"_id": "65d200f3812caeb85e21ee12", "application_name": "app2"},
+        ]
+        mock_find_all_apps.return_value = [
+            {"_id": ObjectId("65d200f3812caeb85e21ee19"), "application_name": "app1"},
+            {"_id": ObjectId("65d200f3812caeb85e21ee12"), "application_name": "app2"},
+        ]
+
+        response = self.client.get("/api/v1/application/")
+        response_data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response_data, expected)
+        # Add more assertions to check the response data
+
+
+if __name__ == "__main__":
+    unittest.main()
