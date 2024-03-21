c4bab62e23c782c74820e8c7576aa437f94a2b6f
Mahmoud ElKodary <melkodary@outlook.com>
2024-03-21 14:39:32 +0100

    Fixing uri of mongodb

--- root_orchestrator/system-manager-python/ext_requests/mongodb_client.py
+++ root_orchestrator/system-manager-python/ext_requests/mongodb_client.py
@@ -5,7 +5,7 @@ from flask_pymongo import PyMongo
 MONGO_URL = os.environ.get("CLOUD_MONGO_URL", "localhost")
 MONGO_PORT = os.environ.get("CLOUD_MONGO_PORT", 10007)
 
-MONGO_ADDR_USERS = "fmongodb://{MONGO_URL}:{MONGO_PORT}/users"
+MONGO_ADDR_USERS = f"mongodb://{MONGO_URL}:{MONGO_PORT}/users"
 
 mongo_users = None
 mongo_organization = None
