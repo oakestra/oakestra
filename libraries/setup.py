17c0daf546773384c0dadbfc7ab714cf6554b7a2
Mahmoud ElKodary <melkodary@outlook.com>
2024-03-27 16:56:24 +0100

    update requirements file

--- libraries/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="resource_abstractor_client",
-    version="0.1.0",
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=["requests==2.27.1"],
-)
