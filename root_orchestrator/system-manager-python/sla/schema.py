062611ad8e12cb93070210de4e9e5a8371ffd129
Giovanni Bartolomeo <giobarty@gmail.com>
2024-04-17 17:43:03 +0200

    updated schema

--- root_orchestrator/system-manager-python/sla/schema.py
+++ root_orchestrator/system-manager-python/sla/schema.py
@@ -148,7 +148,7 @@ sla_schema = {
                         "exclusiveMinimum": 0,
                     },
                 },
-                "required": ["applicationID", "application_name","application_namespace","microservices"],
+                "required": ["applicationID", "application_name", "application_namespace", "microservices"],
             },
         },
         "args": {
