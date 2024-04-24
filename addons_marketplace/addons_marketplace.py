706004180d2a1b706475cfe919e989c634fa72b7
Mahmoud ElKodary <melkodary@outlook.com>
2024-04-24 18:31:37 +0200

    dockerfile for marketplace

--- addons_marketplace/addons_marketplace.py
+++ addons_marketplace/addons_marketplace.py
@@ -37,4 +37,4 @@ def health():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=ADDON_MARKETPLACE_PORT, debug=True)
+    app.run(host="0.0.0.0", port=ADDON_MARKETPLACE_PORT, debug=False)
