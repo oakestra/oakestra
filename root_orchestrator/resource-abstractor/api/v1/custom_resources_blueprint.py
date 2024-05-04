cf8e499362c3dd02b7f0a5a480e20289456f3ec9
Mahmoud ElKodary <melkodary@outlook.com>
2024-05-04 14:02:13 +0200

    custom resources added

---
+++ root_orchestrator/resource-abstractor/api/v1/custom_resources_blueprint.py
@@ -0,0 +1,108 @@
+import json
+
+import jsonschema
+from db import custom_resources_db as custom_resources
+from flask import request
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from marshmallow import INCLUDE, Schema, fields
+from services.hook_service import before_after_hook
+
+customblp = Blueprint("Custom Resources", "custom_resources", url_prefix="/api/v1/custom_resources")
+
+
+class CustomResourceSchema(Schema):
+    _id = fields.String()
+    resource_type = fields.String(required=True)
+    schema = fields.Dict()
+
+
+@customblp.route("/")
+class CustomResourceController(MethodView):
+    @customblp.response(200, CustomResourceSchema(many=True), content_type="application/json")
+    def get(self):
+        return list(custom_resources.find_custom_resources())
+
+    @customblp.arguments(CustomResourceSchema, location="json")
+    @customblp.response(201, CustomResourceSchema, content_type="application/json")
+    def post(self, data, *args, **kwargs):
+        return custom_resources.create_custom_resource(data)
+
+
+@customblp.route("/<resource>")
+class ResourcesController(MethodView):
+    def get(self, *args, **kwargs):
+        resource_type = kwargs.get("resource")
+        filter = request.args.to_dict()
+
+        meta_data = custom_resources.find_custom_resource_by_type(resource_type)
+        if meta_data is None:
+            abort(404, message="Custom Resource not registered")
+
+        result = list(custom_resources.find_resources(resource_type, filter))
+
+        return json.dumps(result, default=str)
+
+    @customblp.arguments(Schema(unknown=INCLUDE), location="json")
+    @before_after_hook()
+    def post(self, data, *args, **kwargs):
+        resource_type = kwargs.get("resource")
+
+        meta_data = custom_resources.find_custom_resource_by_type(resource_type)
+        if meta_data is None:
+            abort(404, message="Custom Resource not registered")
+
+        try:
+            jsonschema.validate(data, meta_data.get("schema", {}))
+        except jsonschema.ValidationError as e:
+            abort(400, message=e.message)
+
+        result = custom_resources.create_resource(resource_type, data)
+
+        return json.dumps(result, default=str)
+
+
+@customblp.route("/<resource>/<resource_id>")
+class ResourceController(MethodView):
+    def get(self, *args, **kwargs):
+        resource_type = kwargs.get("resource")
+        resource_id = kwargs.get("resource_id")
+
+        meta_data = custom_resources.find_custom_resource_by_type(resource_type)
+        if meta_data is None:
+            abort(404, message="Custom Resource not registered")
+
+        result = custom_resources.find_resource_by_id(resource_type, resource_id)
+
+        return json.dumps(result, default=str)
+
+    @customblp.arguments(Schema(unknown=INCLUDE), location="json")
+    @before_after_hook(with_param_id="resource_id")
+    def patch(self, data, *args, **kwargs):
+        resource_type = kwargs.get("resource")
+        resource_id = kwargs.get("resource_id")
+
+        meta_data = custom_resources.find_custom_resource_by_type(resource_type)
+        if meta_data is None:
+            abort(404, message="Custom Resource not found")
+
+        try:
+            jsonschema.validate(data, meta_data.get("schema", {}))
+        except jsonschema.ValidationError as e:
+            abort(400, message=e.message)
+
+        result = custom_resources.update_resource(resource_type, resource_id, data)
+
+        return json.dumps(result, default=str)
+
+    @customblp.response(204)
+    @before_after_hook(with_param_id="resource_id")
+    def delete(self, *args, **kwargs):
+        resource_type = kwargs.get("resource")
+        resource_id = kwargs.get("resource_id")
+
+        meta_data = custom_resources.find_custom_resource_by_type(resource_type)
+        if meta_data is None:
+            abort(404, message="Custom Resource not found")
+
+        custom_resources.delete_resource(resource_type, resource_id)
---
+++ root_orchestrator/resource-abstractor/db/custom_resources_db.py
@@ -0,0 +1,66 @@
+from bson.objectid import ObjectId
+from db import mongodb_client as db
+
+
+def find_custom_resources(filter={}):
+    return db.mongo_meta_data.find(filter)
+
+
+def find_custom_resource_by_type(resource_type):
+    return db.mongo_meta_data.find_one({"resource_type": resource_type})
+
+
+def delete_custom_resource_by_type(resource_type):
+    return db.mongo_meta_data.find_one_and_delete({"resource_type": resource_type})
+
+
+def create_custom_resource(data):
+    data.pop("_id", None)
+
+    inserted = db.mongo_meta_data.insert_one(data)
+
+    return db.mongo_meta_data.find_one({"_id": inserted.inserted_id})
+
+
+def check_resource_type_exists(resource_type):
+    custom_resource = find_custom_resource_by_type(resource_type)
+
+    return custom_resource is not None
+
+
+def _get_collection(resource_type):
+    return db.db_custom_resources.db[resource_type]
+
+
+def find_resources(resource_type, filter={}):
+    collection = _get_collection(resource_type)
+
+    return collection.find(filter)
+
+
+def find_resource_by_id(resource_type, id):
+    collection = _get_collection(resource_type)
+
+    return collection.find_one({"_id": ObjectId(id)})
+
+
+def create_resource(resource_type, data):
+    collection = _get_collection(resource_type)
+
+    inserted = collection.insert_one(data)
+
+    return collection.find_one({"_id": inserted.inserted_id})
+
+
+def update_resource(resource_type, id, data):
+    collection = _get_collection(resource_type)
+
+    return collection.find_one_and_update(
+        {"_id": ObjectId(id)}, {"$set": data}, return_document=True
+    )
+
+
+def delete_resource(resource_type, id):
+    collection = _get_collection(resource_type)
+
+    return collection.find_one_and_delete({"_id": ObjectId(id)})
