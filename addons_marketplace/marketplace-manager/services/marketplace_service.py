4473f263623c8c592a608c247ca6a85b75ac6e4e
Mahmoud ElKodary <melkodary@outlook.com>
2024-05-23 20:30:26 +0200

    deleting image shouldn't fail verification

--- addons_marketplace/marketplace-manager/services/marketplace_service.py
+++ addons_marketplace/marketplace-manager/services/marketplace_service.py
@@ -10,17 +10,23 @@ def verify_addon(addon_id, addon):
     client = docker.from_env()
     for service in addon["services"]:
         image = service.get("image")
+        image_id = None
         try:
             # TODO utilize opencontainers image spec to verify image instead of docker
             logging.info(f"Pulling image: {image}")
-            service = client.images.pull(image)
+            pulled_image = client.images.pull(image)
+            image_id = pulled_image.id
             logging.info(f"Image pulled: {image}")
-
-            client.images.remove(service.id)
         except docker.errors.DockerException as e:
-            logging.warning(f"Failed to pull/remove image {image}: {e}")
+            logging.warning(f"Failed to pull {image}: {e}")
             marketplace_db.update_addon(addon_id, {"status": "verification_failed"})
             return
+
+        try:
+            # This is not a failure, maybe the image is used by another service
+            client.images.remove(image_id)
+        except docker.errors.DockerException as e:
+            logging.warning(f"Failed to remove image {image_id}: {e}")
 
     logging.info(f"Addon-{addon_id} verified")
     marketplace_db.update_addon(addon_id, {"status": "approved"})
