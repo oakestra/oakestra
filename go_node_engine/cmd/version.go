1f63fc338a73929443bcab221a399e05df34d80f
Giovanni Bartolomeo <giovanni.bartolomeo.rg@gmail.com>
2024-07-29 18:19:04 +0200

    fix Version

--- go_node_engine/cmd/version.go
+++ go_node_engine/cmd/version.go
@@ -17,6 +17,6 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of NodeEngine",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(version)
+		fmt.Println(Version)
 	},
 }
