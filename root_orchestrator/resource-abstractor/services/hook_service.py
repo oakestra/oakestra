7ec3ad4af29ec1125689591948e9743a7ad63aac
Mahmoud ElKodary <melkodary@outlook.com>
2024-08-28 10:50:19 +0200

    use pre post instead of before & after

--- root_orchestrator/resource-abstractor/services/hook_service.py
+++ root_orchestrator/resource-abstractor/services/hook_service.py
@@ -41,16 +41,16 @@ def process_async_hook(entity_name, event, entity_id):
         threading.Thread(target=call_webhook, args=(hook["webhook_url"], data)).start()
 
 
-def after_create(entity_name, entity_id):
-    process_async_hook(entity_name, hooks_db.HookEventsEnum.AFTER_CREATE.value, entity_id)
+def process_post_create(entity_name, entity_id):
+    process_async_hook(entity_name, hooks_db.HookEventsEnum.POST_CREATE.value, entity_id)
 
 
-def after_update(entity_name, entity_id):
-    process_async_hook(entity_id, entity_name, hooks_db.HookEventsEnum.AFTER_UPDATE.value)
+def process_post_update(entity_name, entity_id):
+    process_async_hook(entity_id, entity_name, hooks_db.HookEventsEnum.POST_UPDATE.value)
 
 
-def after_delete(entity_name, entity_id):
-    process_async_hook(entity_id, entity_name, hooks_db.HookEventsEnum.AFTER_DELETE.value)
+def process_post_delete(entity_name, entity_id):
+    process_async_hook(entity_id, entity_name, hooks_db.HookEventsEnum.POST_DELETE.value)
 
 
 def process_sync_hook(entity_name, event, data):
@@ -62,52 +62,52 @@ def process_sync_hook(entity_name, event, data):
     return data
 
 
-def before_create(entity_name, data):
-    return process_sync_hook(entity_name, hooks_db.HookEventsEnum.BEFORE_CREATE.value, data)
+def process_pre_create(entity_name, data):
+    return process_sync_hook(entity_name, hooks_db.HookEventsEnum.PRE_CREATE.value, data)
 
 
-def before_update(entity_name, data):
-    return process_sync_hook(entity_name, hooks_db.HookEventsEnum.BEFORE_UPDATE.value, data)
+def process_pre_update(entity_name, data):
+    return process_sync_hook(entity_name, hooks_db.HookEventsEnum.PRE_UPDATE.value, data)
 
 
 def perform_create(entity_name, fn, *args):
     data = args[-1]
-    data = before_create(entity_name, data)
+    data = process_pre_create(entity_name, data)
 
     args = list(args)
     args[-1] = data
 
     result = fn(*tuple(args))
-    after_create(entity_name, str(result.get("_id")))
+    process_post_create(entity_name, str(result.get("_id")))
 
     return result
 
 
 def perform_update(entity_name, fn, *args):
     data = args[-1]
-    data = before_update(entity_name, data)
+    data = process_pre_update(entity_name, data)
 
     # Update the last argument with the new data
     args = list(args)
     args[-1] = data
 
     result = fn(*tuple(args))
-    after_update(entity_name, str(result.get("_id")))
+    process_post_update(entity_name, str(result.get("_id")))
 
     return result
 
 
 # put currently is not supported
 api_hooks_map = {
-    "post": (before_create, after_create),
-    "patch": (before_update, after_update),
-    "delete": (None, after_delete),
+    "post": (process_pre_create, process_post_create),
+    "patch": (process_pre_update, process_post_update),
+    "delete": (None, process_post_delete),
 }
 
 
 # doesn't work well with @arguments decorator
 # this decorator has to be the closest to the method
-def before_after_hook(entity_name, with_param_id=None):
+def pre_post_hook(entity_name, with_param_id=None):
     def decorator(fn):
         @wraps(fn)
         def wrapper(*args, **kwargs):
@@ -118,16 +118,16 @@ def before_after_hook(entity_name, with_param_id=None):
             # incase this was wrapped by @arguments
             data = args[1] if len(args) > 1 else request.json
 
-            before_fn = api_hooks_map[method_name][0]
-            after_fn = api_hooks_map[method_name][1]
+            pre_fn = api_hooks_map[method_name][0]
+            post_fn = api_hooks_map[method_name][1]
 
             entity_id = kwargs.get(with_param_id) if with_param_id else None
 
-            if before_fn:
+            if pre_fn:
                 if entity_id:
                     data["_id"] = entity_id
 
-                data = before_fn(entity_name, data)
+                data = pre_fn(entity_name, data)
 
             args = list(args)
             if data and len(args) > 1:
@@ -149,7 +149,7 @@ def before_after_hook(entity_name, with_param_id=None):
                 # This happens with delete methods that don't return the deleted entity
                 result_id = entity_id
 
-            after_fn(entity_name, result_id)
+            post_fn(entity_name, result_id)
 
             return result
 
