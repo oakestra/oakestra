7107115a747cf83268aea592df1478cd20933907
Mahmoud ElKodary <m.elkodary@tum.de>
2024-11-13 17:30:37 +0100

    hooks added (#320)
    * hooks added
    
    * custom resources added
    
    * remove return document
    
    * testing...
    
    * delete hook fixed + requirements update
    
    * fix endpoint
    
    * resource deletion + some bug fixing
    
    * patch request for hooks
    
    * fixing hooks missing id with event
    
    * enum rename + some schema definitions
    
    * trick pipeline
    
    * remove docker compose tests
    
    * find available entities undo
    
    * use pre post instead of before & after
    
    * pre_post_hook name issue fixed
    
    * fix update and delete operations
    
    * sanity check
    
    * formatting
    
    * fix jobs patch request
    
    * linting stuff
    
    ---------
    
    Co-authored-by: Giovanni Bartolomeo <giobarty@gmail.com>
    Co-authored-by: Giovanni Bartolomeo <giovanni.bartolomeo.rg@gmail.com>

--- root_orchestrator/resource-abstractor/api/v1/resources_blueprint.py
+++ root_orchestrator/resource-abstractor/api/v1/resources_blueprint.py
@@ -5,9 +5,10 @@ from db.jobs_db import find_job_by_id
 from flask.views import MethodView
 from flask_smorest import Blueprint
 from marshmallow import INCLUDE, Schema, fields
+from services.hook_service import perform_create, perform_update, pre_post_hook
 from werkzeug import exceptions
 
-resourcesblp = Blueprint("Resources Info", "resources_info", url_prefix="/api/v1/resources")
+resourcesblp = Blueprint("Resources", "resources", url_prefix="/api/v1/resources")
 
 
 class ResourceSchema(Schema):
@@ -63,25 +64,33 @@ class AllResourcesController(MethodView):
         return list(clusters_db.find_clusters(filter))
 
     @resourcesblp.arguments(ResourceSchema(unknown=INCLUDE), location="json")
+    @resourcesblp.response(201, ResourceSchema, content_type="application/json")
+    @pre_post_hook("resources")
+    def post(self, data, **kwargs):
+        return clusters_db.create_cluster(data)
+
+    @resourcesblp.arguments(ResourceSchema(unknown=INCLUDE), location="json")
     @resourcesblp.response(200, ResourceSchema, content_type="application/json")
     def put(self, data, **kwargs):
         resource_name = data.get("cluster_name")
 
         cluster = clusters_db.find_cluster_by_name(resource_name)
         if cluster:
-            return clusters_db.update_cluster(str(cluster["_id"]), data)
+            return perform_update(
+                "resources", clusters_db.update_cluster, str(cluster["_id"]), data
+            )
 
-        return clusters_db.create_cluster(data)
+        return perform_create("resources", clusters_db.create_cluster, data)
 
 
-@resourcesblp.route("/<resourceId>")
+@resourcesblp.route("/<resource_id>")
 class ResourceController(MethodView):
     @resourcesblp.response(200, ResourceSchema, content_type="application/json")
-    def get(self, resourceId):
-        if ObjectId.is_valid(resourceId) is False:
+    def get(self, resource_id):
+        if ObjectId.is_valid(resource_id) is False:
             raise exceptions.BadRequest()
 
-        cluster = clusters_db.find_cluster_by_id(resourceId)
+        cluster = clusters_db.find_cluster_by_id(resource_id)
         if cluster is None:
             raise exceptions.NotFound()
 
@@ -89,10 +98,18 @@ class ResourceController(MethodView):
 
     @resourcesblp.arguments(ResourceSchema(unknown=INCLUDE), location="json")
     @resourcesblp.response(200, ResourceSchema, content_type="application/json")
+    @pre_post_hook("resources", with_param_id="resource_id")
     def patch(self, data, **kwargs):
-        resource_id = kwargs.get("resourceId")
+        resource_id = kwargs.get("resource_id")
 
         if ObjectId.is_valid(resource_id) is False:
             raise exceptions.BadRequest()
 
         return clusters_db.update_cluster_information(resource_id, data)
+
+    @resourcesblp.response(204, ResourceSchema, content_type="application/json")
+    @pre_post_hook("resources", with_param_id="resource_id")
+    def delete(self, *args, **kwargs):
+        resource_id = kwargs.get("resource_id")
+
+        clusters_db.delete_cluster(resource_id)
---
+++ root_orchestrator/resource-abstractor/services/hook_service.py
@@ -0,0 +1,169 @@
+import json
+import logging
+import os
+import threading
+from functools import wraps
+
+from db import hooks_db
+from flask import request
+from requests import exceptions, post
+
+RESPONSE_TIMEOUT = os.environ.get("HOOK_REQUEST_TIMEOUT", 5)
+CONNECT_TIMEOUT = os.environ.get("HOOK_CONNECT_TIMEOUT", 10)
+
+
+def call_webhook(url, data):
+    try:
+        logging.info(f"Calling webhook with data: {data}")
+        response = post(url, json=data, timeout=(CONNECT_TIMEOUT, RESPONSE_TIMEOUT))
+        response.raise_for_status()
+        try:
+            # in async mode we don't care about the response
+            data = response.json()
+        except json.JSONDecodeError:
+            pass
+
+    except exceptions.ConnectTimeout as e:
+        logging.warning(f"Request timed out while trying to connect to {url}", exc_info=e)
+    except exceptions.ReadTimeout as e:
+        logging.warning(
+            f"{url} failed to return response in the allotted amount of time", exc_info=e
+        )
+    except exceptions.RequestException as e:
+        logging.warning(f"Hook failed to send request to {url}", exc_info=e)
+
+    # if request fails the original data is returned
+    return data
+
+
+def process_async_hook(entity_name, event, entity_id):
+    async_hooks = hooks_db.find_hooks({"entity": entity_name, "events": {"$in": [event]}})
+
+    for hook in async_hooks:
+        data = {
+            "entity": entity_name,
+            "entity_id": entity_id,
+            "event": event,
+        }
+        threading.Thread(target=call_webhook, args=(hook["webhook_url"], data)).start()
+
+
+def process_post_create(entity_name, entity_id):
+    process_async_hook(entity_name, hooks_db.HookEventsEnum.POST_CREATE.value, entity_id)
+
+
+def process_post_update(entity_name, entity_id):
+    process_async_hook(entity_name, hooks_db.HookEventsEnum.POST_UPDATE.value, entity_id)
+
+
+def process_post_delete(entity_name, entity_id):
+    process_async_hook(entity_name, hooks_db.HookEventsEnum.POST_DELETE.value, entity_id)
+
+
+def process_sync_hook(entity_name, event, data):
+    sync_hooks = hooks_db.find_hooks({"entity": entity_name, "events": {"$in": [event]}})
+
+    for hook in sync_hooks:
+        data = call_webhook(hook["webhook_url"], data)
+
+    return data
+
+
+def process_pre_create(entity_name, data):
+    return process_sync_hook(entity_name, hooks_db.HookEventsEnum.PRE_CREATE.value, data)
+
+
+def process_pre_update(entity_name, data):
+    return process_sync_hook(entity_name, hooks_db.HookEventsEnum.PRE_UPDATE.value, data)
+
+
+def perform_create(entity_name, fn, *args):
+    data = args[-1]
+    data = process_pre_create(entity_name, data)
+
+    args = list(args)
+    args[-1] = data
+
+    result = fn(*tuple(args))
+    process_post_create(entity_name, str(result.get("_id")))
+
+    return result
+
+
+def perform_update(entity_name, fn, *args):
+    data = args[-1]
+    data = process_pre_update(entity_name, data)
+
+    # Update the last argument with the new data
+    args = list(args)
+    args[-1] = data
+
+    result = fn(*tuple(args))
+    process_post_update(entity_name, str(result.get("_id")))
+
+    return result
+
+
+# put currently is not supported
+api_hooks_map = {
+    "post": (process_pre_create, process_post_create),
+    "patch": (process_pre_update, process_post_update),
+    "delete": (None, process_post_delete),
+}
+
+
+# doesn't work well with @arguments decorator
+# this decorator has to be the closest to the method
+def pre_post_hook(name=None, with_param_id=None):
+    def decorator(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            method_name = fn.__name__
+            if method_name not in api_hooks_map.keys():
+                raise ValueError(f"Method {method_name} not supported")
+
+            # incase this was wrapped by @arguments
+            data = args[1] if len(args) > 1 else request.json
+
+            pre_fn = api_hooks_map[method_name][0]
+            post_fn = api_hooks_map[method_name][1]
+
+            entity_id = kwargs.get(with_param_id) if with_param_id else None
+            entity_name = name if name else kwargs.get("resource")  # for custom resources
+            if not entity_name:
+                raise ValueError("Couldn't determine entity name.")
+
+            if pre_fn:
+                if entity_id:
+                    data["_id"] = entity_id
+
+                data = pre_fn(entity_name, data)
+
+            args = list(args)
+            if data and len(args) > 1:
+                args[1] = data
+            elif data:
+                args.append(data)
+
+            result = fn(*tuple(args), **kwargs)
+            result_id = str(result["_id"]) if isinstance(result, dict) else None
+
+            # incase result was json encoded
+            if result and result_id is None:
+                try:
+                    result_id = json.loads(result).get("_id")
+                except Exception:
+                    pass
+            elif result_id is None:
+                # fallback on passed id
+                # This happens with delete methods that don't return the deleted entity
+                result_id = entity_id
+
+            if result_id:
+                post_fn(entity_name, result_id)
+
+            return result
+
+        return wrapper
+
+    return decorator
