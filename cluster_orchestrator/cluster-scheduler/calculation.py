5a1aa2bc6e46160e43ce0e081df6b3282e14dc62
giobarty <giobarty@gmail.com>
2024-11-18 17:28:39 +0100

    linting

--- cluster_orchestrator/cluster-scheduler/calculation.py
+++ cluster_orchestrator/cluster-scheduler/calculation.py
@@ -100,10 +100,8 @@ def greedy_load_balanced_algorithm(
         try:
             if does_node_respects_requirements(extract_specs(node), job):
                 qualified_nodes.append(node)
-        except Exception as e:
+        except Exception:
             logging.error("[ERROR] Skipping a node. Node info extraction error")
-
-
 
     target_node = None
     target_cpu = 0
