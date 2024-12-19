2c7f5d12866da20e55954a53813294d10601f675
Malyuk-A <malyuk.alexander1999@gmail.com>
2024-12-19 10:25:30 +0100

    Fix install scripts

--- scripts/InstallOakestraWorker.sh
+++ scripts/InstallOakestraWorker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 if [ -z "$OAKESTRA_VERSION" ]; then
     OAKESTRA_VERSION=$(curl -s https://raw.githubusercontent.com/oakestra/oakestra/main/version.txt)
 else 
@@ -12,13 +13,21 @@ echo Installing Oakestra Node Engine and Net Manager version $OAKESTRA_VERSION
 rm NodeEngine_$(dpkg --print-architecture).tar.gz 2> /dev/null
 rm NetManager_$(dpkg --print-architecture).tar.gz 2> /dev/null
 
-wget -c https://github.com/oakestra/oakestra/releases/download/$OAKESTRA_VERSION/NodeEngine_$(dpkg --print-architecture).tar.gz && tar -xzf NodeEngine_$(dpkg --print-architecture).tar.gz && chmod +x install.sh && ./install.sh $(dpkg --print-architecture)
+wget -c https://github.com/oakestra/oakestra/releases/download/$OAKESTRA_VERSION/NodeEngine_$(dpkg --print-architecture).tar.gz &&
+    tar -xzf NodeEngine_$(dpkg --print-architecture).tar.gz &&
+    chmod +x install.sh &&
+    ./install.sh $(dpkg --print-architecture)
+
 if [ $? -ne 0 ]; then
         echo "Error: Failed to retrieve or install the Oakestra Node Engine."
         exit 1
 fi
 
-wget -c https://github.com/oakestra/oakestra-net/releases/download/$OAKESTRA_VERSION/NetManager_$(dpkg --print-architecture).tar.gz && tar -xzf NetManager_$(dpkg --print-architecture).tar.gz && chmod +x install.sh && ./install.sh $(dpkg --print-architecture)
+wget -c https://github.com/oakestra/oakestra-net/releases/download/$OAKESTRA_VERSION/NetManager_$(dpkg --print-architecture).tar.gz &&
+    tar -xzf NetManager_$(dpkg --print-architecture).tar.gz &&
+    chmod +x install.sh &&
+    ./install.sh $(dpkg --print-architecture)
+
 if [ $? -ne 0 ]; then
         echo "Error: Failed to retrieve or install the Oakestra Net Manager."
         exit 1
