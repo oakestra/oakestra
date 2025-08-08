51a23cf654c7a197cdd05301b52add8db4bfb984
giobarty <giobarty@gmail.com>
2025-08-08 12:43:47 +0200

    ruff format

--- cluster_orchestrator/cluster-manager/blueprints/__init__.py
+++ cluster_orchestrator/cluster-manager/blueprints/__init__.py
@@ -2,8 +2,4 @@ from blueprints.service_blueprints import serviceblp, schedulingblp
 from blueprints.worker_blueprints import workerblp
 
 
-blueprints = [
-    serviceblp,
-    schedulingblp,
-    workerblp
-]
+blueprints = [serviceblp, schedulingblp, workerblp]
