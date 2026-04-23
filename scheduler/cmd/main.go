d05ba3bd77d83f0cba115f89242d4be94f1d96b9
Matthew Humphreys <ge65yay@tum.de>
2026-04-23 08:40:19 +0200

    rename scheduling algorithm
    Signed-off-by: Matthew Humphreys <ge65yay@tum.de>

--- scheduler/cmd/main.go
+++ scheduler/cmd/main.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"log"
+	"os"
 	"scheduler/api"
+	"scheduler/logger"
+	"strconv"
 )
 
+func setup() {
+	debugMode, err := strconv.ParseBool(os.Getenv("DEBUG"))
+	if err == nil && debugMode {
+		logger.SetDebugMode()
+	}
+}
+
 func main() {
+	setup()
 	go func() {
 		StartTaskQueueServer()
 		log.Fatal("Task queue server stopped")
